e1a844a7da66c350b08d6d9a7417f167cf944dde
altantheprodigy <altanpoke9@gmail.com>
2024-09-21 11:06:23 +0700

    fix ui

--- lib/pages/SearchPage/widgets/search_card.dart
+++ lib/pages/SearchPage/widgets/search_card.dart
@@ -21,67 +21,62 @@ class SearchPageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        Get.toNamed("/detail-page", arguments: id);
-      },
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 7),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: AppColors.background),
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: AppResponsive().screenWidth(context) * 0.20,
-              width: AppResponsive().screenWidth(context) * 0.30,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12.0),
-                child: Image.network(
-                  imagePath,
-                  fit: BoxFit.cover,
-                ),
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 7),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: AppColors.background),
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: AppResponsive().screenWidth(context) * 0.20,
+            width: AppResponsive().screenWidth(context) * 0.30,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Image.network(
+                imagePath,
+                fit: BoxFit.cover,
               ),
             ),
-            Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      gameName,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: AppTextStyle()
-                          .descriptionBold(context, AppColors.fontAppBar),
-                    ),
-                    Text(
-                      DateFormat('dd-MM-yyyy').format(releaseDate),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: AppTextStyle()
-                          .descriptionBold(context, AppColors.cardIconFill),
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.star,
-                          size: 20,
-                          color: AppColors.bintang,
-                        ),
-                        Text(
-                          rating.toString(),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: AppTextStyle()
-                              .descriptionBold(context, AppColors.cardIconFill),
-                        ),
-                      ].withSpaceBetween(width: 5),
-                    )
-                  ],
-                ))
-          ].withSpaceBetween(width: 10),
-        ),
+          ),
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    gameName,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: AppTextStyle()
+                        .descriptionBold(context, AppColors.fontAppBar),
+                  ),
+                  Text(
+                    DateFormat('dd-MM-yyyy').format(releaseDate),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: AppTextStyle()
+                        .descriptionBold(context, AppColors.cardIconFill),
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        size: 20,
+                        color: AppColors.bintang,
+                      ),
+                      Text(
+                        rating.toString(),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: AppTextStyle()
+                            .descriptionBold(context, AppColors.cardIconFill),
+                      ),
+                    ].withSpaceBetween(width: 5),
+                  )
+                ],
+              ))
+        ].withSpaceBetween(width: 10),
       ),
     );
   }
